621d721c63fe17c1214c81299bc6e6a762683e6e
Iriskyto <ira-silich@mail.ru>
2022-02-03 22:23:16 +0300

    MultiPlayer Project Files added

---
+++ Source/DedicatedServer/DedicatedServer.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DedicatedServer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DedicatedServer, "DedicatedServer" );
+ 
---
+++ Source/DedicatedServer/DedicatedServerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DedicatedServerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADedicatedServerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADedicatedServerGameMode();
+};
+
+
+
